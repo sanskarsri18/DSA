487fb9fb1ae4c1bf951733a61e2ac624e53c78de
Sanskar Srivastava <99880907+sanskarsri18@users.noreply.github.com>
2026-09-06 23:14:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (88.91%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        int n = s.size();
+        
+        stack<char> st;
+        for(int i = 0; i < n; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(s[i] == ')' && st.top() != '('){
+                    return false;
+                }
+                else if(s[i] == '}' && st.top() != '{'){
+                    return false;
+                }
+                else if(s[i] == ']' && st.top() != '['){
+                    return false;
+                }
+                else{
+                    st.pop();
+                }
+            }
+        }
+        if(st.size() != 0){
+            return false;
+        }
+        return true;
+    }
+};
