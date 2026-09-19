2a2378156d15d548dfb285b12d091e6ef7ae4747
Sanskar Srivastava <99880907+sanskarsri18@users.noreply.github.com>
2026-09-19 21:34:42 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (47.55%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
-        int n = arr.size();
-        long long noDel = arr[0];
-        long long oneDel = LLONG_MIN;
-        long long res = arr[0];
-
+        long long int n = arr.size();
+        long long int noDel = arr[0];
+        long long int oneDel = INT_MIN;
+        long long int ans = arr[0];
         for(int i = 1; i < n; i++){
-            long long prevNoDel = noDel;
-            noDel = max((long long)arr[i], noDel + arr[i]);
-
-            if (oneDel == LLONG_MIN) {
-                oneDel = prevNoDel;              // first time: only option is "delete arr[i]"
-            } else {
-                oneDel = max(prevNoDel, oneDel + arr[i]);
-            }
-
-            res = max(res, max(noDel, oneDel));
+            long long int prevnoDel = noDel;
+            long long int prevoneDel = oneDel;
+            long long int val1 = max(prevnoDel + arr[i], (long long)arr[i]);
+            long long int val2 = max(prevoneDel + arr[i], prevnoDel);
+            noDel = val1;
+            oneDel = val2;
+            ans = max(ans, max(val1, val2));
         }
-        return (int)res;
+        return ans;
     }
 };
